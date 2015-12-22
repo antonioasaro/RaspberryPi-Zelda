d3eb8edee8b4df16cfed2147fca95bf83e9b8fe9
Antonio Asarom <antonio_asaro@hotmail.com>
2015-12-22 09:43:26 -0500

    Works ok with ftp gaps

--- ftp.js
+++ ftp.js
@@ -1,18 +1,45 @@
 var JSFtp = require('jsftp');
+var fs = require('fs');
 console.log('ftp - echo');
 
 var Ftp = new JSFtp({
     host: "ftp.asarotools.com",
     port: 21,
     user: "raspberrypi@asarotools.com",
-    pass: "***"
+    pass: "****"
 });
 
-Ftp.ls(".", function(err, res) {
-    res.forEach(function(file) {
-	    console.log(file.name);
+var disconnect = function() {
+    Ftp.raw.quit(function(err, data) {
+    	console.log("quit");
+   	});
+}
+
+var ls = function() {
+   	console.log("Try 'ls' cmd");
+    Ftp.ls(".", function(err, res) {
+		if (!err) {
+        	console.log("'ls' successful");
+            res.forEach(function(file) {
+        	    console.log(file.name);
+	            disconnect();
+            });
+		}
     });
-	Ftp.raw.quit(function(err, data) {
-		console.log("Bye!");
-	});
+}
+
+var put = function() {
+    Ftp.put("./zelda.log", "./zelda.log", function(hadError) {
+        if (!hadError) {
+    	    console.log("File transferred successfully");
+		} else {
+    	    console.log("File transfer failed: " + hadError);
+		}
+	    disconnect();
+    });
+}
+
+fs.watchFile('zelda.log', function(curr, prev) {
+	console.log('Ftp zelda.log at ' + curr.mtime);
+	put();
 });
